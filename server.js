2bfac180cd88784083f33f267d98291bad52eb0d
Maggie may <maggiemaywilder@gmail.com>
2021-03-01 17:10:59 -0600

    corrections to server functions

--- server.js
+++ server.js
@@ -4,16 +4,15 @@ const path = require('path');
 const uuid = require('uuid');
 const fs = require('fs');
 
-const db = require('./db/db.json');
-
 const PORT = process.env.PORT || 3000;
 const app = express();
+const db = 'db/db.json';
 
 // middleware to log: look at adding time and saving log (moment.js/29:00 in travesy video) 
 const logger = (req, res, next) => {
     console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
     next();
-}
+};
 
 app.use(express.urlencoded({ extended: true })); // handles url encoded data
 app.use(express.json()); //parses json data
@@ -25,7 +24,7 @@ app.use(logger);
 // // return index.html
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-})
+});
 
 // // return notes.html
 app.get("/notes", (req, res) => {
@@ -34,31 +33,54 @@ app.get("/notes", (req, res) => {
 
 // read the db.json file and return save notes as JSON
 app.get("/api/notes", (req, res) => {
-    return res.json(db);
-})
+    let notes;
+    fs.readFile(db, 'utf-8', (err, data) => {
+        if (err) throw err;
+        notes = JSON.parse(data)
+        return res.json(notes);
+    });
+});
 
+// save content from note edit, assign id, pull notes and add new, rewrite file with new included
 app.post("/api/notes", (req, res) => {
     const newNote = {
         "id": uuid.v4(), 
         "title": req.body.title,
         "text": req.body.text,
     };
-    const notes = db;
-    notes.push(newNote);
-    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
-        if(err) throw err;
-        console.log('File has been saved.');
-        return res.json(newNote);
-    });
-    // do I need to do something to rewrite the file?.... yes? pretty sure
-})
+    let notes;
+    fs.readFile(db, 'utf-8', (err, data) => {
+        if (err) throw err;
+        notes = JSON.parse(data)
+        notes.push(newNote);
 
+        fs.writeFile(db, JSON.stringify(notes), (err) => {
+            if(err) throw err;
+            console.log('File has been rewritten');
+            return res.json(notes);
+        });
+    });
+});
+
+// receive the id of note to delete, check all notes fro db.json, remove that one and rewrite the notes to db.json
 app.delete("/api/notes/:id", (req, res) => {
     const deleteId = req.params.id;
-    const notes = db;
-    console.log(db);
-//     // receive the id of note to delete, check all notes fro db.json, remove that one and rewrite the notes to db.json
-})
+    let notes;
+    fs.readFile(db, 'utf-8', (err, data) => {
+        if (err) throw err;
+        notes = JSON.parse(data)
+        for(let i = 0; i<notes.length; i++) {
+            if(notes[i].id === deleteId) {
+                notes.splice(i,1);
+            };
+        };
+        fs.writeFile(db, JSON.stringify(notes), (err) => {
+            if(err) throw err;
+            console.log('File has been rewritten');
+            return res.json(notes);
+        });
+    });
+});
 
 
 
